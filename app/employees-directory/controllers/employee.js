3305b4efb3561802d01668e0a59a7ee65dfbf936
Quinten Pohl <quinten.pohl@gmail.com>
2016-04-01 00:15:49 +0100

    Added files via upload

---
+++ app/employees-directory/controllers/employee.js
@@ -0,0 +1,14 @@
+angular.module('app')
+    .controller('EmployeeCtrl', ['$scope', '$routeParams', 'Employee', 'Report', 
+	 function ($scope, $routeParams, Employee, Report) {	
+        if (!$routeParams.employeeId) {
+          console.info('Visited list of employees');
+          $scope.employees = Employee.query();
+        } else if ($routeParams.employeeId && !$routeParams.reports) {
+          console.info('Visited details of employeeId: ' + $routeParams.employeeId);
+          $scope.employee = Employee.get({employeeId: $routeParams.employeeId});          
+        } else {
+          console.info('Visited reports of employeeId: ' + $routeParams.employeeId);
+          $scope.employees = Report.query({employeeId: $routeParams.employeeId});          
+        }		
+    }]);
---
+++ app/factories.js
@@ -0,0 +1,22 @@
+angular.module('app')
+    .factory('Employee', ['employeesService',
+      function (employeesService) {
+          return {
+              query: function () {
+                  return employeesService.getAllEmployees();
+              },
+              get: function (employee) {
+                  return employeesService.findById(parseInt(employee.employeeId));
+              }
+          }
+
+      }])
+    .factory('Report', ['employeesService',
+        function (employeesService) {
+            return {
+                query: function (employee) {
+                    return employeesService.findByManager(parseInt(employee.employeeId));
+                }
+            }
+
+        }]);
