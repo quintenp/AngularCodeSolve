3c732df29164ce915f662baa8da29e50030c5f8e
Quinten Pohl <Quinten_pohl@dell.com>
2016-04-01 13:57:55 +0100

    navigation directives logic enhancements

--- app/directives.js
+++ app/directives.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('employeeLink', ['$location', '$rootScope', function($location, $rootScope) {
+angular.module('app').directive('employeeGoLink', ['$location', '$rootScope', function($location, $rootScope) {
     return {
         scope: {
             empid: '=',
@@ -7,44 +7,65 @@ angular.module('app').directive('employeeLink', ['$location', '$rootScope', func
         },
         transclude: true,
         template: function(element, attrs) {
-            var type = attrs.type || 'text';
-            var hasLinkType = attrs.hasOwnProperty('linktype');
-            var linktype = attrs.linktype;
-
-            if (linktype === 'report') {
+            if (attrs.linktype === 'report') {
                 return '<a href=""  ng-bind="reports"></a>';
             }
-            else if (linktype === 'details') {
-                return '<button class="btn btn-primary"><span class="badge">{{reports}}</span><span class="glyphicon glyphicon-chevron-right"></span></button>';
-            }
-            else if (linktype === 'manager') {
-                return '<a href="">{{manager}}</a>';
+            else if (attrs.linktype === 'manager') {
+                return '<a href="" ng-bind="manager"></a>';
             }
         },
         link: function(scope, element, attrs) {
             element.on('click', function() {
-                var type = attrs.type || 'text';
-                var hasLinkType = attrs.hasOwnProperty('linktype');
-                var linktype = attrs.linktype;
-                
                 var employeePath = '/employees/';
                 var reporPath = '/reports';
-                
                 var path = '';
-                
-                if (linktype === 'report') {
-                    $rootScope.go('/employees/' + scope.empid + '/reports');
+
+                if (attrs.linktype === 'report') {
                     path = employeePath + scope.empid + reporPath;
                 }
-                else if (linktype === 'details' || linktype === 'manager') {
-                    $rootScope.go('/employees/' + scope.empid);
+                else if (attrs.linktype === 'manager') {
                     path = employeePath + scope.empid;
                 }
-              
-                $location.url(path)
-                scope.$apply();
+
+                scope.$apply(function() {
+                    $rootScope.slider = 'slider-left';
+                    $location.path(path);
+                });
             });
         }
-
     };
+}]).directive('employeeGoButton', ['$location', '$rootScope', function($location, $rootScope) {
+    return {
+        scope: {
+            empid: '=',
+            reports: '='
+        },
+        transclude: true,
+        template: '<button class="btn btn-primary"><span class="badge" ng-bind="reports"></span><span class="glyphicon glyphicon-chevron-right"></span></button>',
+        link: function(scope, element, attrs) {
+            element.on('click', function() {
+                var employeePath = '/employees/';
+                var path = '';
+                
+                path = employeePath + scope.empid;
+
+                scope.$apply(function() {
+                    $rootScope.slider = 'slider-left';
+                    $location.path(path);
+                });
+            });
+        }
+    };
+}]).directive('backButton', ['$window', '$rootScope', function($window, $rootScope) {
+    return {
+        template: '<button class="btn btn-primary"><span class="glyphicon glyphicon-chevron-left"></span></button>',
+        link: function(scope, element, attrs) {
+            element.on('click', function() {
+                scope.$apply(function() {
+                    $rootScope.slider = 'slider-right';
+                    $window.history.back();
+                });
+            });
+        }
+    }
 }]);
--- app/employees-directory/controllers/main.js
+++ app/employees-directory/controllers/main.js
@@ -1,12 +1,5 @@
 angular.module('app')
     .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location',
         function($scope, $rootScope, $window, $location) {
-            $scope.slider = '';
-            $rootScope.back = function() {
-                $scope.slider = 'slider-right';
-                $window.history.back();
-            }
-            $rootScope.go = function(path) {
-                $scope.slider = 'slider-left';
-            }
+
         }]);
