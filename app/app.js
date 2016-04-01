5336255dc89404d82a2e5a9cfd6e308cb7af0e42
Quinten Pohl <Quinten_pohl@dell.com>
2016-04-01 12:57:00 +0100

    Initial formatting done

--- app/app.js
+++ app/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
-angular.module('app',[
+angular.module('app', [
     'ngTouch',
     'ngRoute',
     'ngAnimate'
 ]).
-config(['$routeProvider', function ($routeProvider){
-	
-    var genericRT = {
-      templateUrl: 'app/employees-directory/views/container.html', 
-      controller: 'EmployeeCtrl'
-    },detailRT = {
-      templateUrl: 'app/employees-directory/views/templates/employeeDetails.html', 
-      controller: 'EmployeeCtrl'
-    },reportRT = {
-      templateUrl: 'app/employees-directory/views/templates/employeeReport.html', 
-      controller: 'EmployeeCtrl'
-    };
-	
-  $routeProvider.when('/employees', genericRT);
-  $routeProvider.when('/employees/:employeeId', detailRT);
-  $routeProvider.when('/employees/:employeeId/:reports', reportRT);
-  $routeProvider.otherwise({redirectTo: '/employees'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+
+        var genericRT = {
+            templateUrl: 'app/employees-directory/views/container.html',
+            controller: 'EmployeeCtrl'
+        }, detailRT = {
+            templateUrl: 'app/employees-directory/views/templates/employeeDetails.html',
+            controller: 'EmployeeCtrl'
+        }, reportRT = {
+            templateUrl: 'app/employees-directory/views/templates/employeeReport.html',
+            controller: 'EmployeeCtrl'
+        };
+
+        $routeProvider.when('/employees', genericRT);
+        $routeProvider.when('/employees/:employeeId', detailRT);
+        $routeProvider.when('/employees/:employeeId/:reports', reportRT);
+        $routeProvider.otherwise({ redirectTo: '/employees' });
+    }]);
--- app/employees-directory/controllers/main.js
+++ app/employees-directory/controllers/main.js
@@ -1,14 +1,12 @@
 angular.module('app')
-    .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', 
-	 function ($scope, $rootScope, $window, $location) {
-        $scope.slider = '';
-        $rootScope.back = function()
-        {
-          $scope.slider = 'slider-right';
-          $window.history.back();
-        }
-        $rootScope.go = function(path)
-        {
-          $scope.slider = 'slider-left';
-        }
-    }]);
+    .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location',
+        function($scope, $rootScope, $window, $location) {
+            $scope.slider = '';
+            $rootScope.back = function() {
+                $scope.slider = 'slider-right';
+                $window.history.back();
+            }
+            $rootScope.go = function(path) {
+                $scope.slider = 'slider-left';
+            }
+        }]);
