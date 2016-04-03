944fb361c13b9925eb1b23743facf4fd4ddd6df1
Quinten Pohl <quinten.pohl@gmail.com>
2016-04-03 21:01:36 +0100

    Refactor + logic improvements

--- app/app.js
+++ app/app.js
@@ -2,21 +2,23 @@ angular.module('app', [
     'ngTouch',
     'ngRoute',
     'ngAnimate'
-]).config(['$routeProvider', function ($routeProvider) {
-        'use strict';
-        var genericRT = {
-            templateUrl: 'app/employees-directory/views/container.html',
-            controller: 'EmployeeCtrl'
-        }, detailRT = {
-            templateUrl: 'app/employees-directory/views/employeeDetails.html',
-            controller: 'EmployeeCtrl'
-        }, reportRT = {
-            templateUrl: 'app/employees-directory/views/employeeReport.html',
-            controller: 'EmployeeCtrl'
-        };
+]).config(['$routeProvider','ROUTES',function($routeProvider,ROUTES) {
+    'use strict';
 
-        $routeProvider.when('/employees', genericRT);
-        $routeProvider.when('/employees/:employeeId', detailRT);
-        $routeProvider.when('/employees/:employeeId/:reports', reportRT);
-        $routeProvider.otherwise({ redirectTo: '/employees' });
-    }]);
+    var genericRT = {
+        templateUrl: 'app/employees-directory/views/employees.html',
+        controller: 'EmployeeCtrl',
+    }, detailRT = {
+        templateUrl: 'app/employees-directory/views/employeeDetails.html',
+        controller: 'EmployeeCtrl'
+    }, reportRT = {
+        templateUrl: 'app/employees-directory/views/employeeReport.html',
+        controller: 'EmployeeCtrl'
+    };
+
+    $routeProvider
+        .when(ROUTES.EMPLOYEE_DEFAULT.url, genericRT)
+        .when(ROUTES.EMPLOYEE_DETAILS.url, detailRT)
+        .when(ROUTES.REPORT_LIST.url, reportRT)
+        .otherwise({ redirectTo: ROUTES.EMPLOYEE_DEFAULT.url });
+}]);
